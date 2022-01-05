3bde8fe6d983c04df901842338b8c7d7deeff1e6
AyushVanpariya <ayushvanparia@gmail.com>
2022-01-05 22:29:00 +0530

    update

---
+++ Classes/Inheritance.py
@@ -0,0 +1,20 @@
+class Animal:
+    def __init__(self):
+        self.age = 1
+
+    def eat(self):
+        print("Eat")
+
+
+class Mammal(Animal):
+    def walk():
+        print("walk")
+
+
+class Fish(Animal):
+    def swim():
+        print("swim")
+
+
+m = Mammal()
+print(m.age)
---
+++ Classes/Object_class.py
@@ -0,0 +1,24 @@
+# object class is base class for all class in python
+
+class Animal:
+    def __init__(self):
+        self.age = 1
+
+    def eat(self):
+        print("Eat")
+
+
+class Mammal(Animal):
+    def walk():
+        print("walk")
+
+
+class Fish(Animal):
+    def swim():
+        print("swim")
+
+
+m = Mammal()
+print(isinstance(m, object))
+# issubclass() checks if a class is derived from another class
+print(issubclass(Animal, object))
