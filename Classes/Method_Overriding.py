ca3bc8c6e01bf240e478b64ad265a63d3eae3d52
AyushVanpariya <ayushvanparia@gmail.com>
2022-01-05 22:34:55 +0530

    update

---
+++ Classes/Method_Overriding.py
@@ -0,0 +1,27 @@
+class Animal:
+    def __init__(self):
+        print("This is Animal constructor")
+        self.age = 1
+
+    def eat(self):
+        print("Eat")
+
+
+class Mammal(Animal):
+    def __init__(self):
+        print("This is mammel constructor")
+        self.weight = 1
+        super().__init__()
+
+    def walk():
+        print("walk")
+
+
+class Fish(Animal):
+    def swim():
+        print("swim")
+
+
+m = Mammal()
+print(m.age)
+print(m.weight)
