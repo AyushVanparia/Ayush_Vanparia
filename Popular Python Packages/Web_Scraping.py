89e1f6a396ab434b548e9745a447a8e2e4f7d96a
AyushVanpariya <ayushvanparia@gmail.com>
2022-01-10 21:53:39 +0530

    Update Web_Scraping.py

--- Popular Python Packages/Web_Scraping.py
+++ Popular Python Packages/Web_Scraping.py
@@ -5,4 +5,9 @@ reponse = requests.get("https://stackoverflow.com/questions")
 soup = BeautifulSoup(reponse.text, "html.parser")
 
 questions = soup.select(".question-summary")
-print(questions[0].get("id", 0))
+# print(questions[0].get("id", 0))# getting attribute
+# print(questions[0].select(".question-hyperlink")) # returns a list
+# print(questions[0].select_one(".question-hyperlink"))  # returns single object
+for question in questions:
+    print(question.select_one(".question-hyperlink").get_text())
+    print(question.select_one(".vote-count-post").get_text())
