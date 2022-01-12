ca9815af13dc0c1b752494f80453c3130648f0aa
AyushVanpariya <ayushvanparia@gmail.com>
2022-01-12 12:46:14 +0530

    update

--- Popular Python Packages/Browser_Automation.py
+++ Popular Python Packages/Browser_Automation.py
@@ -2,19 +2,19 @@ from selenium import webdriver
 
 
 browser = webdriver.Chrome()
-browser.get("https://github.com")
+browser.get("https://gmail.com")
 
 # username = browser.find_element_by_id("identifierId")
 # username.send_keys("ayushvanpariya07@gmail.com")
 # next_btn = browser.find_element_by_class_name("VfPpkd-vQzf8d")
 # next_btn.click()
-Signin_link = browser.find_element_by_link_text("Sign in")
-Signin_link.click()
-username = browser.find_element_by_id("login_field")
-username.send_keys("test1@gmail.com")
-password = browser.find_element_by_id("password")
-password.send_keys("asdf")
-password.submit()
+# Signin_link = browser.find_element_by_link_text("Sign in")
+# Signin_link.click()
+# username = browser.find_element_by_id("login_field")
+# username.send_keys("ayushvanparia@gmail.com")
+# password = browser.find_element_by_id("password")
+# password.send_keys("qwe456yuioP")
+# password.submit()
 
 
 # assert "AyushVanparia" in browser.page_source
@@ -23,4 +23,4 @@ password.submit()
 # linklabel = pflink.get_attribute("innerHTML")
 # assert "AyushVanparia" in linklabel
 
-browser.quit()
+# browser.quit()
