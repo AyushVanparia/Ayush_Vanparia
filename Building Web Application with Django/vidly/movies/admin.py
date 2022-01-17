8bd4cac21680b35807f39c8a4711feab3ff0bf1d
AyushVanpariya <ayushvanparia@gmail.com>
2022-01-17 23:51:02 +0530

    update

--- Building Web Application with Django/vidly/movies/admin.py
+++ Building Web Application with Django/vidly/movies/admin.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.contrib import admin
 from .models import Genre, Movie
 # Register your models here.
@@ -7,5 +8,10 @@ class GenreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
 
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ('date_created',)
+    list_display = ('title', 'number_in_stock', 'daily_rate')
+
+
 admin.site.register(Genre, GenreAdmin)
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
